d377ab9158c8e5668630c5525bf40314182797ce
Jibbyworks <JDBucog@yahoo.com>
2021-07-20 21:50:19 -0700

    initial commit, no js active

---
+++ resources/js/script.js
@@ -0,0 +1,25 @@
+const testimonials = [    
+  {
+    "username": "Tanya Sinclair",
+    "imgURL": "/images/image-tanya.jpg",
+    "testimonial": "/“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”/",
+    "title": "UX Engineer"
+  },
+  {
+    "username": "John Tarkpor",
+    "imgURL": "/images/image-john.jpg",
+    "testimonial": "/“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”/",
+    "title": "Junior Front-end Developer"
+  }
+]
+
+// DOM elements
+const username = document.getElementById('username')
+const testimonial = document.getElementById('testimonial')
+const profession = document.getElementById('profession')
+const sliderImage = document.getElementById('slider-image')
+
+// Arrows
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+
