ac2313e72e8c5744a23236195ff97779c99aa5b0
Jibbyworks <JDBucog@yahoo.com>
2021-07-21 17:32:37 -0700

    finalize styles and js

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,24 +2,49 @@ const testimonials = [
   {
     "username": "Tanya Sinclair",
     "imgURL": "/images/image-tanya.jpg",
-    "testimonial": "/“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”/",
-    "title": "UX Engineer"
+    "testimonial": "“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”",
+    "profession": "UX Engineer"
   },
   {
     "username": "John Tarkpor",
     "imgURL": "/images/image-john.jpg",
-    "testimonial": "/“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”/",
-    "title": "Junior Front-end Developer"
+    "testimonial": "“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”",
+    "profession": "Junior Front-end Developer"
   }
 ]
 
-// DOM elements
-const username = document.getElementById('username')
-const testimonial = document.getElementById('testimonial')
-const profession = document.getElementById('profession')
-const sliderImage = document.getElementById('slider-image')
-
 // Arrows
-const prev = document.getElementById('prev')
-const next = document.getElementById('next')
+const sliderArrows = document.getElementById('slider-arrows')
+let pos = 0
 
+
+sliderArrows.addEventListener('click', changeTestimonial)
+
+function changeTestimonial(e) {
+  const username = document.getElementById('username')
+  const testimonial = document.getElementById('testimonial')
+  const profession = document.getElementById('profession')
+  const sliderImage = document.getElementById('slider-image')
+
+  if(e.target.id === 'prev') {
+    console.log('prev')
+    if(pos === 0){
+      pos = testimonials.length - 1
+    } else if(pos > 0 && pos <= testimonials.length - 1) {
+      pos--
+    }
+  } else if (e.target.id === 'next') {
+    console.log('next')
+    if(pos >= 0 && pos < testimonials.length - 1) {
+      pos++
+    } else if(pos === testimonials.length - 1) {
+      pos = 0
+    }
+  }
+  
+  username.innerHTML = testimonials[pos].username
+  profession.textContent = testimonials[pos].profession
+  testimonial.innerHTML = testimonials[pos].testimonial
+  sliderImage.src = testimonials[pos].imgURL
+  sliderImage.alt = testimonials[pos].username
+}
